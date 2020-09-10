a467023e572d518c2af36de837e44c482f45d516
Alessio Bonetti <alesine87@gmail.com>
2020-09-10 12:51:23 +0200

    setTime

--- js/script.js
+++ js/script.js
@@ -3,19 +3,37 @@ function sendMessage() {
 
   if(inputText != ""){
     var templateMessage = $(".template .message_row").clone();
-
-    var date = new Date();
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var time = hours + ":" + minutes;
+    var time = getTime();
 
     templateMessage.find(".message_text").text(inputText);
     templateMessage.find("message_time").text(time);
     templateMessage.addClass("sent");
 
     $(".chat_messages").append(templateMessage);
+    setTimeout(cpuMessage,1000);
     $("#input_message").val("");
   }
+}
+
+function cpuMessage(){
+  var cpuMessage = $(".templates .message_row").clone();
+
+  cpuMessage.find(".message_text").text("Ok");
+  var time = getTime();
+  cpuMessage.find(".message_time").text(time);
+
+  $(".chat_messages").append(cpuMessage);
+}
+
+function getTime() {
+  var date = new Date();
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+
+  if (minutes < 10){
+    minutes= "0" + minutes;
+  }
+  return  hours + ":" + minutes;
 }
 
 
